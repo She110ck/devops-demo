3232badf846b267b4a4562a7b48c928fb3dea98b
unknown <rusifbudaqov@gmail.com>
2021-08-09 23:25:15 +0400

    db add

--- src/main/java/com/example/demo/SpringController.java
+++ src/main/java/com/example/demo/SpringController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,5 +17,10 @@ public class SpringController {
     @GetMapping("/index")
     public List<Person> hello() {
         return personRepository.findAll();
+    }
+
+    @PostMapping("/add")
+    public void save(@RequestBody Person person) {
+         personRepository.save(person);
     }
 }
