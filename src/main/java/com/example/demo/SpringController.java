d94ff71dbf60872fd97b672a4e6b405777f39f34
unknown <rusifbudaqov@gmail.com>
2021-08-09 23:22:03 +0400

    db

--- src/main/java/com/example/demo/SpringController.java
+++ src/main/java/com/example/demo/SpringController.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SpringController {
 
+    @Autowired
+    private PersonRepository personRepository;
+
     @GetMapping("/index")
-    public String hello(){
-        return "Hello Jimi";
+    public List<Person> hello() {
+        return personRepository.findAll();
     }
 }
